4efc88c21f20d7605dbc77671586735b756049c7
Yashvardhan Sharma <97288349+yourstruelyyash@users.noreply.github.com>
2024-03-22 19:04:05 +0530

    Update app.py
    removed sender_id and receiver_id

--- CipherChat/app.py
+++ CipherChat/app.py
@@ -38,27 +38,18 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    date_of_birth = db.Column(db.Date, nullable=False)  # Use Date type for date_of_birth
-    sender_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    receiver_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    profile_picture = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    date_of_birth = db.Column(db.String(50), nullable=False)
+    profile_picture = db.Column(db.Text, nullable=True)
     timestamp = db.Column(db.DateTime, default=db.func.now(), nullable=False)
-    content = db.Column(db.String(255), nullable=False, default="")
 
-    sent_messages = db.relationship("Message", back_populates="sender", lazy=True, foreign_keys="Message.sender_id")
-    received_messages = db.relationship("Message", back_populates="receiver", lazy=True, foreign_keys="Message.receiver_id")
-
-    def __init__(self, username, password, name, email, date_of_birth, profile_picture, content, sender_id, receiver_id):
+    def __init__(self, username, password, name, email, date_of_birth, profile_picture):
         self.username = username
         self.password = password
         self.name = name
         self.email = email
         self.date_of_birth = date_of_birth
         self.profile_picture = profile_picture
-        self.content = content
-        self.sender_id = sender_id
-        self.receiver_id = receiver_id
 
     def check_password(self, entered_password):
         return self.password == entered_password
