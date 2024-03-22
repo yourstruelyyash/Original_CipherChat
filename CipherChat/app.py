5510fe2c20ba41942d2e1a05b246920a2d8d56a5
Yashvardhan Sharma <97288349+yourstruelyyash@users.noreply.github.com>
2024-03-22 14:25:04 +0530

    Update app.py

--- CipherChat/app.py
+++ CipherChat/app.py
@@ -461,4 +461,4 @@ def load_user(user_id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    socketio.run(app, host="0.0.0.0", port=10000, debug=True, use_reloader=False, allow_unsafe_werkzeug=True)
+    socketio.run(app)
